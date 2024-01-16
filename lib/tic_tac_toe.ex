ff6a88a1d67b18116c8eae5bd6673eb79af7ae37
Bernardo Amorim <contato@bamorim.com>
2024-01-16 22:53:07 +0000

    Add basic play action

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -10,4 +10,12 @@ defmodule TicTacToe do
   def new_game do
     %Game{winner: nil, turn: :x, x: MapSet.new(), o: MapSet.new()}
   end
+
+  def play(%Game{turn: :x, x: plays} = game, x, y) do
+    {:ok, %Game{game | turn: :o, x: MapSet.put(plays, {x, y})}}
+  end
+
+  def play(%Game{turn: :o, o: plays} = game, x, y) do
+    {:ok, %Game{game | turn: :x, o: MapSet.put(plays, {x, y})}}
+  end
 end
--- test/tic_tac_toe_test.exs
+++ test/tic_tac_toe_test.exs
@@ -10,4 +10,24 @@ defmodule TicTacToeTest do
       assert %Game{winner: nil, turn: :x, x: ^empty, o: ^empty} = TicTacToe.new_game()
     end
   end
+
+  describe "play/3" do
+    test "adds a play to the x player play list" do
+      game = TicTacToe.new_game()
+
+      assert {:ok, %Game{turn: :o} = new_game} = TicTacToe.play(game, 0, 0)
+      assert new_game.o == game.o
+      assert MapSet.size(new_game.x) == 1
+      assert {0, 0} in new_game.x
+    end
+
+    test "adds a play to the o player play list" do
+      {:ok, game} = TicTacToe.new_game() |> TicTacToe.play(0, 0)
+
+      assert {:ok, %Game{turn: :x} = new_game} = TicTacToe.play(game, 1, 0)
+      assert new_game.x == game.x
+      assert MapSet.size(new_game.o) == 1
+      assert {1, 0} in new_game.o
+    end
+  end
 end
